72b03d6ffb67005a5cab4bd79728d363b6749910
Hajime MATSUMOTO <hajime.matsumoto@avap.co.jp>
2015-02-19 14:53:22 +0900

    Environmentクラスを作成

---
+++ src/modules/Environment/class/Environment.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Nora Project
+ *
+ * @author Hajime MATSUMOTO <hajime@nora-worker.net>
+ * @copyright 2015 nora-worker.net.
+ * @licence https://www.nora-worker.net/LICENCE
+ * @version 1.0.0
+ */
+namespace Nora\Module\Environment;
+
+use Nora\Core\Component\Component;
+use Nora\Core\Util\Collection\Hash;
+
+/**
+ * Environmentクラス
+ */
+class Environment extends Component
+{
+
+    public function initComponentImple ( )
+    {
+        $this->_SERVER = Hash::create(
+            Hash::NO_CASE_SENSITIVE, $_SERVER
+        );
+    }
+
+    public function register ( )
+    {
+        // ハンドラ系
+        set_error_handler([$this, 'phpErrorHandler']);
+        set_exception_handler([$this, 'phpExceptionHandler']);
+        register_shutdown_function([$this, 'phpShutdownHandler']);
+        return $this;
+    }
+
+    /**
+     *  int $errno , string $errstr [, string $errfile [, int $errline [, array $errcontext ]
+     */
+    public function phpErrorHandler($errno, $errstr, $errfile, $errline, $errcontext)
+    {
+        $this->fire('php.error', [
+            'errno'      => $errno,
+            'errstr'     => $errstr,
+            'errfile'    => $errfile,
+            'errline'    => $errline,
+            'errcontext' => $errcontext
+        ]);
+    }
+
+    /**
+     */
+    public function phpExceptionHandler($exception)
+    {
+        $this->fire('php.exception', [
+            'exception' => $exception
+        ]);
+    }
+
+    /**
+     */
+    public function phpShutdownHandler( )
+    {
+        $error = error_get_last();
+        if ($error['type'] == 1) {
+            $this->phpErrorHandler(
+                $error['errno'],
+                $error['errstr'],
+                $error['errfile'],
+                $error['errline'],
+                $error['errcontext']
+            );
+        }
+        $this->fire('php.shutdown');
+    }
+
+    /**
+     * サーバ変数を取得
+     */
+    public function _SERVER( )
+    {
+        return $this->_SERVER;
+    }
+}
--- src/modules/Environment/class/Facade.php
+++ src/modules/Environment/class/Facade.php
@@ -18,56 +18,14 @@ class Facade extends Module
 {
     protected function initModuleImple( )
     {
+        $this->rootScope( )->setComponent('Environment', function ( ) {
+            return new Environment($this->scope());
+        });
     }
 
     public function register ( )
     {
-        // ハンドラ系
-        set_error_handler([$this, 'phpErrorHandler']);
-        set_exception_handler([$this, 'phpExceptionHandler']);
-        register_shutdown_function([$this, 'phpShutdownHandler']);
-
-        return $this;
-    }
-
-    /**
-     *  int $errno , string $errstr [, string $errfile [, int $errline [, array $errcontext ]
-     */
-    public function phpErrorHandler($errno, $errstr, $errfile, $errline, $errcontext)
-    {
-        $this->fire('php.error', [
-            'errno'      => $errno,
-            'errstr'     => $errstr,
-            'errfile'    => $errfile,
-            'errline'    => $errline,
-            'errcontext' => $errcontext
-        ]);
-    }
-
-    /**
-     */
-    public function phpExceptionHandler($exception)
-    {
-        $this->fire('php.exception', [
-            'exception' => $exception
-        ]);
-    }
-
-    /**
-     */
-    public function phpShutdownHandler( )
-    {
-        $error = error_get_last();
-        if ($error['type'] == 1) {
-            $this->phpErrorHandler(
-                $error['errno'],
-                $error['errstr'],
-                $error['errfile'],
-                $error['errline'],
-                $error['errcontext']
-            );
-        }
-        $this->fire('php.shutdown');
+        return $this->Environment()->register();
     }
 
     protected function afterConfigure( )
